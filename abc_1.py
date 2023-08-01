8a0f625b6cf5398aab733636e57a05be156ac459
Vaishnavi Patil <vaishnavipatil0407@gmail.com>
2023-08-01 08:06:20 +0530

    add this files

--- abc_1.py
+++ abc_1.py
@@ -1,6 +1,9 @@
-from flask import Flask
-from flask import request
+from flask import Flask, request, render_template, jsonify
+
 app = Flask(__name__)
+@app.route('/', methods= ['GET','POST'])
+def home_page():
+    return render_template("index.html")
 
 @app.route("/")
 def hello_world():
@@ -11,19 +14,18 @@ def test():
     a= 5+6
     return "this is my function to run{}".format(a)
 
-@app.route("/test2/test2")
-def test2():
-    data = request.args.get("x")
-    return "this is a data input from my url {}".format(data)
+# @app.route("/test2/test2")
+# def test2():
+#     data = request.args.get("x")
+#     return "this is a data input from my url {}".format(data)
 
-@app.route("login")
+@app.route("/login")
 def login():
      data1 = 45+8
      return "login my page from url {}". format(data1)
 
 if __name__=="__main__":
     app.run(host="0.0.0.0")
-
 
 
 
